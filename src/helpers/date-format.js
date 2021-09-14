1eef056b7c3cc4e57549170baffa7fcebcf15b39
Matko Setnik <matko.setnik@mstart.hr>
2021-09-14 16:25:15 +0200

    Dorada prikaza datuma

--- src/helpers/date-format.js
+++ src/helpers/date-format.js
@@ -1,7 +1,8 @@
 export const formatDate = (date) => {
   const d = new Date(date);
-  const year = d.getYear();
+  const year = d.getFullYear();
   const month = d.getMonth() + 1;
   const day = d.getDate();
+
   return `${day}/${month}/${year}`;
 };
